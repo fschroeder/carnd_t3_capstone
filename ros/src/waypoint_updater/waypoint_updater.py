0761357cec0da636141103197a89f0b1c3e8ce0d
Fabian Schroeder <fabian.schroeder@gmail.com>
2019-03-10 20:19:01 +0100

    Started with Docker

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -111,8 +111,8 @@ class WaypointUpdater(object):
             stop_idx = max(self.stopline_wp_idx - closest_idx - 2, 0)
             dist = self.distance(waypoints, i, stop_idx)
             vel = math.sqrt(2 * MAX_DECEL * dist)
-            if(vel < 1.):
-                vel = 0
+            if(vel < 1.0):
+                vel = 0.0
 
             p.twist.twist.linear.x = min(vel, wp.twist.twist.linear.x)
             temp.append(p)
